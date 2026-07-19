924ead0c7471b05564bafd3491d42dd313cbea1f
pmarques097-netizen <pmarques097@gmail.com>
2026-07-19 14:30:33 -0400

    Delete INSTALAR_DEPENDENCIAS.bat

--- INSTALAR_DEPENDENCIAS.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist ".venv\Scripts\python.exe" (
-  py -3.12 -m venv .venv 2>nul || py -3.11 -m venv .venv 2>nul || python -m venv .venv
-)
-".venv\Scripts\python.exe" -m pip install --upgrade pip
-".venv\Scripts\python.exe" -m pip install -r requirements.txt
-pause
