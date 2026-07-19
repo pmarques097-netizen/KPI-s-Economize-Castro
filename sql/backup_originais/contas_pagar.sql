62021bbe620520390180ef7785faf62e6a3ba230
pmarques097-netizen <pmarques097@gmail.com>
2026-07-19 15:07:30 -0300

    Add files via upload

---
+++ sql/backup_originais/contas_pagar.sql
@@ -0,0 +1,10 @@
+-- CONTAS A PAGAR
+-- Consulta editável pela interface.
+-- Use :data_inicio e :data_fim conforme o período da meta.
+SELECT
+    NULL::text AS unidade,
+    NULL::text AS fornecedor,
+    NULL::text AS plano_contas,
+    NULL::date AS data_vencimento,
+    NULL::numeric AS valor_documento
+WHERE 1 = 0;
---
+++ sql/backup_originais/vendas.sql
@@ -0,0 +1,86 @@
+SELECT
+    unidadenegocio.nome AS loja,
+    unidadenegocio.codigo AS numero_loja,
+    us_orc.apelido AS usuario_orcamento,
+    venda.id AS vendaid,
+    venda.status AS status_venda,
+    embalagem.descricao,
+    embalagem.etiqueta,
+    embalagem.codigobarras,
+    produto.codigo AS cod_interno,
+    classificacao.caminho AS classificacao_3_nivel,
+    CASE
+        WHEN classificacao.caminho LIKE '%>%- %'
+            THEN SPLIT_PART(classificacao.caminho, '>', 2)
+        ELSE classificacao.caminho
+    END AS classificacao_resumida,
+    itemvenda.quantidade,
+    itemvenda.valorunitario,
+    itemorcamento.precovenda,
+    me.custo AS custo,
+    me.quantidade AS qtd_mov,
+    itemvenda.desconto,
+    itemvenda.valortotal,
+    (itemvenda.valortotal - me.custo * itemvenda.quantidade) AS lucro,
+    CASE
+        WHEN itemvenda.valortotal <> 0
+        THEN (
+            itemvenda.valortotal - me.custo * itemvenda.quantidade
+        ) / itemvenda.valortotal
+        ELSE 0
+    END AS lucro_perc,
+    venda.datahorafechamento AS datahora_venda_final,
+    me.datahora,
+    venda.cpfcnpj,
+    itemvenda.status AS status_item_venda,
+    pbm_primario.nome AS programa_pbm,
+    pbm_secundario.nome AS programa_pbm_secundario,
+    CASE
+        WHEN 'PEC - FIDELIDADE' IN (
+            pbm_primario.nome,
+            pbm_secundario.nome
+        )
+        THEN 'PEC'
+        ELSE 'NAO-PEC'
+    END AS pec,
+    venda.coo
+FROM itemvenda
+JOIN venda
+    ON venda.id = itemvenda.vendaid
+   AND venda.status = 'F'
+JOIN embalagem
+    ON itemvenda.embalagemid = embalagem.id
+LEFT JOIN produto
+    ON embalagem.produtoid = produto.id
+LEFT JOIN classificacaoproduto
+    ON produto.id = classificacaoproduto.produtoid
+LEFT JOIN classificacao
+    ON classificacaoproduto.classificacaoid = classificacao.id
+   AND classificacao.principal = TRUE
+LEFT JOIN itemorcamento
+    ON itemvenda.itemorcamentoid = itemorcamento.id
+LEFT JOIN orcamento
+    ON itemorcamento.orcamentoid = orcamento.id
+LEFT JOIN usuario us_orc
+    ON us_orc.id = itemorcamento.usuarioid
+LEFT JOIN usuario us_ven
+    ON us_ven.id = venda.usuarioid
+LEFT JOIN unidadenegocio
+    ON unidadenegocio.id = itemvenda.unidadenegocioid
+LEFT JOIN orcamentopbm
+    ON orcamento.id = orcamentopbm.orcamentoid
+LEFT JOIN pbm pbm_primario
+    ON orcamentopbm.pbmid = pbm_primario.id
+LEFT JOIN pbm pbm_secundario
+    ON pbm_secundario.id = orcamentopbm.pbmsecundariaid
+LEFT JOIN movimentacaoestoque me
+    ON itemvenda.movimentacaoestoqueid = me.id
+   AND itemvenda.datahora = me.datahora
+   AND me.unidadenegocioid = itemvenda.unidadenegocioid
+WHERE me.tipomovimentacaoestoqueid = 26
+  AND itemvenda.status = 'F'
+  AND me.datahora BETWEEN :data_inicio AND :data_fim
+  AND COALESCE(classificacao.caminho, '') NOT ILIKE '%USO CONSUMO%'
+  AND unidadenegocio.codigo NOT IN (
+      '14-2', '24-2', '26', '40', '41', 'BKP', 'CLOUD', 'ESC'
+  );
